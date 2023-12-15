60f05204c7543e255158483c270d539a2ca670eb
Joseph Ampah <josephampah18@gmail.com>
2023-12-15 17:16:05 +0000

    push to libe v1

--- app/Notifications/EmailVerificationNotifier.php
+++ app/Notifications/EmailVerificationNotifier.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class EmailVerificationNotifier extends Notification implements ShouldQueue
+class EmailVerificationNotifier extends Notification
 {
     use Queueable;
 
